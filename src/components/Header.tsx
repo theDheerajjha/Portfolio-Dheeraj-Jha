571cccbc2492476e506dc96aea5fedff35e8fd0d
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2025-01-12 03:35:51 +0530

    WIP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,7 +39,7 @@ const Header: React.FC = () => {
 
   const technologies = [
     'React', 'Vue', 'TypeScript', 
-    'NextJS', 'NodeJS', ''
+    'NextJS', 'NodeJS', 'Tailwind CSS',
   ];
 
   return (
