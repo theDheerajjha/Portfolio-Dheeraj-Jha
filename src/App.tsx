0099e276f6f31e1e0cabeb9b64079a82995fd7d8
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2025-01-15 23:59:40 +0530

    WIP - refactored using prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import About from './components/About';
-import Experience from './components/Experience';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import './index.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import "./index.css";
 
 const App: React.FC = () => {
   return (
