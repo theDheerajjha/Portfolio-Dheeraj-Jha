b04bc768a7d027b169b52471fdc911b19dbb701b
Dheeraj Jha <52864945+theDheerajjha@users.noreply.github.com>
2025-02-02 23:58:15 +0530

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,10 +5,12 @@ declare module "*.jpg" {
   const src: string;
   export default src;
 }
+
 declare module "*.png" {
   const src: string;
   export default src;
 }
+
 declare module "*.svg" {
   const src: string;
   export default src;
