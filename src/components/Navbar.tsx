266c4b3da7ba2444943ec46e14b30a398f78a222
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2025-05-17 23:36:12 +0530

    fixed navbar prohect link not working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -44,7 +44,7 @@ const Navbar: React.FC = () => {
     { name: "Home", href: "#home" },
     { name: "About", href: "#about" },
     { name: "Skills", href: "#skills" },
-    { name: "Projects", href: "#projects" },
+    { name: "Projects", href: "#personal-projects" },
     { name: "Experience", href: "#experience" },
     { name: "Contact", href: "#contact" },
   ];
