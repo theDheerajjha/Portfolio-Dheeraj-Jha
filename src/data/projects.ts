9ab7632f942b74ffa9959a1d5b0d38407457f24d
thedheerajjha <write4dheeraj@gmail.com>
2025-08-06 08:27:37 +0530

    Fixed Project section

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "WebMD - PageBuilder",
-    image: "/src/assets/Pgebuilder-CMS-Webmd.jpg",
+    image: "https://media.licdn.com/dms/image/v2/D562DAQFdoZldhld16g/profile-treasury-image-shrink_8192_8192/B56ZcNmxyqHEAo-/0/1748279959709?e=1755054000&v=beta&t=DX1xrat_BvIKaG36UJcs2PeA8IL9hhc8W7HorzIOWZc",
     challenge: "Enable non-technical users to design and publish content easily within a CMS.",
     approach: "Built dynamic frontend components and enhanced deployment pipeline. Leveraged in-house AI tools (Asterix & Athena) to automate component creation, debugging, and test writing.",
     tools: ["React", "Vue 3", "TypeScript", "Vite", "Asterix AI", "Athena AI"],
@@ -10,7 +10,7 @@ export const projects = [
   },
   {
     title: "Axxess Home Care Platform",
-    image: "/src/assets/Axxess Home care.jpg",
+    image: "https://media.licdn.com/dms/image/v2/D562DAQF-cse6K0bfBw/profile-treasury-image-shrink_8192_8192/B56ZcNpCh4HgAk-/0/1748280552672?e=1755054000&v=beta&t=Q61mOL9DtvxS8kuG5kKOPmxKTp5qNQpXtWQQjjZnx70",
     challenge: "Streamline operations for 1000+ US-based healthcare agencies.",
     approach: "Delivered core features like visit scheduling, care plans, and availability tracking. Built responsive, pixel-perfect UI components and improved data syncing via optimized API integration.",
     tools: ["React", "Next.js", "Tailwind CSS", "Material UI", "REST APIs"],
@@ -19,7 +19,7 @@ export const projects = [
   },
   {
     title: "Wolken's Desk - Service Management App",
-    image: "/src/assets/Wolken Servicedesk.jpg",
+    image: "https://media.licdn.com/dms/image/v2/D562DAQH7ZqbbNK-OwQ/profile-treasury-image-shrink_800_800/B56ZcNrPl0HoAY-/0/1748281130772?e=1755054000&v=beta&t=3sFKd5kqwc_6lze8lxOybfpiluYOzzOWeJocefTLo8U",
     challenge: "Improve ticket management, notifications, and workflow automation for a SaaS-based service desk.",
     approach: "Developed and enhanced features, implemented server-side pagination, and optimized performance for faster data rendering.",
     tools: ["React", "TypeScript", "Axios", "Material UI", "SCSS"],
